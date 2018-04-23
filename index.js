0ee2574e378f8018b4fe1d9e8770f3848cec6d7e
harim1206 <harim1206@gmail.com>
2018-04-23 11:53:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+// [1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
+
+function findMatching(array, name){
+  return array.filter(function(element){ return element.toLowerCase() === name.toLowerCase()});
+}
+
+function fuzzyMatch(array, name){
+  return array.filter(function(element){ return element.toLowerCase()[0] === name.toLowerCase()[0]});
+}
+
+function matchName(array, name){
+  return array.filter(function(element){ return element['name'].toLowerCase() === name.toLowerCase()});
+}
